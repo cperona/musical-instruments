126a990bb15650c4645d331b4d92e20ab422a71f
Christian P <113365748+cperona@users.noreply.github.com>
2026-02-04 13:54:05 +0100

    feat: add static block to Instrument

--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -2,5 +2,9 @@ public abstract class Instrument {
     public String name;
     public int price;
 
+    static {
+        System.out.println("Instruments playing: \n");
+    }
+
     abstract void play();
 }
